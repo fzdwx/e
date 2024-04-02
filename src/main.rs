4ebaafc33b464798a5125f0e3d1d202b0954931f
like <likelovec@gmail.com>
2024-04-02 09:46:02 +0800

    feat: read stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io::{Read, stdin};
+
 fn main() {
-    println!("Hello, world!");
+    let mut buf = [0; 1024];
+    loop {
+        let result = stdin().read(&mut buf);
+        match result {
+            Ok(0) => break,
+            Ok(n) => {
+                let s = String::from_utf8_lossy(&buf[..n]);
+                print!("{}", s);
+            }
+            Err(e) => {
+                eprintln!("error: {}", e);
+                break;
+            }
+        }
+    }
 }
